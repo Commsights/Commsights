f4fb466566be8757417e733c02d9b99d22e2d385
lethanhson1234 <tinhnghesohu@gmail.com>
2020-12-29 08:01:39 +0700

    CRM

--- Commsights.MVC/Controllers/ProductController.cs
+++ Commsights.MVC/Controllers/ProductController.cs
@@ -731,29 +731,10 @@ namespace Commsights.MVC.Controllers
             List<Config> listConfig = _configResposistory.GetSQLWebsiteByGroupNameAndCodeAndActiveAndIsMenuLeftAndRowBeginAndRowEndToList(AppGlobal.CRM, AppGlobal.Website, true, true, indexBegin, indexEnd);
             foreach (Config item in listConfig)
             {
-                Thread t = new Thread(() =>
-                {
-                    this.AsyncCreateProductScanWebsiteNoFilterProduct0001(item);
-                });
-                t.Start();
+                await this.AsyncCreateProductScanWebsiteNoFilterProduct0001(item);
             }
             return "";
-        }
-        public string ScanWebsitePriorityNoFilterProductByIndexBeginVoid001(int indexBegin)
-        {
-            indexBegin = indexBegin + 1;
-            int indexEnd = indexBegin + 4;
-            List<Config> listConfig = _configResposistory.GetSQLWebsiteByGroupNameAndCodeAndActiveAndIsMenuLeftAndRowBeginAndRowEndToList(AppGlobal.CRM, AppGlobal.Website, true, true, indexBegin, indexEnd);
-            foreach (Config item in listConfig)
-            {
-                Thread t = new Thread(() =>
-                {
-                    CreateProductScanWebsiteNoFilterProduct00001(item);
-                });
-                t.Start();
-            }
-            return "";
-        }
+        }        
         public async Task<string> AsyncDecode()
         {
             List<ProductCompact001> list = _productRepository.GetProductCompact001BySourceWithIDAndTitleToList(AppGlobal.SourceAuto);
@@ -1303,127 +1284,7 @@ namespace Commsights.MVC.Controllers
                 string mes = e.Message;
             }
             return "";
-        }
-        public string CreateProductScanWebsiteNoFilterProduct00001(Config config)
-        {
-            try
-            {
-                if (config != null)
-                {
-                    List<LinkItem> list = new List<LinkItem>();
-                    AppGlobal.LinkFinder001(config.URLFull, config.URLFull, true, list);
-                    foreach (LinkItem linkItem in list)
-                    {
-                        try
-                        {
-                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(linkItem.Href);
-                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                            if (response.StatusCode == HttpStatusCode.OK)
-                            {
-                                Stream receiveStream = response.GetResponseStream();
-                                StreamReader readStream = null;
-                                readStream = new StreamReader(receiveStream, Encoding.UTF8);
-                                string html = readStream.ReadToEnd();
-                                html = html.Replace(@"~", @"");
-                                html = AppGlobal.HTMLReplaceAndSplit(html);
-                                string title = "";
-                                string htmlTitle = html;
-                                if ((htmlTitle.Contains(@"<meta property=""og:title"" content=""") == true) || (htmlTitle.Contains(@"<meta property='og:title' content='") == true))
-                                {
-                                    htmlTitle = htmlTitle.Replace(@"<meta property=""og:title"" content=""", @"~");
-                                    htmlTitle = htmlTitle.Replace(@"<meta property='og:title' content='", @"~");
-                                    if (htmlTitle.Split('~').Length > 1)
-                                    {
-                                        htmlTitle = htmlTitle.Split('~')[1];
-                                        htmlTitle = htmlTitle.Replace(@"""", @"~");
-                                        htmlTitle = htmlTitle.Replace(@"'", @"~");
-                                        htmlTitle = htmlTitle.Split('~')[0];
-                                        title = htmlTitle.Trim();
-                                    }
-                                }
-                                else
-                                {
-                                    MatchCollection m1 = Regex.Matches(htmlTitle, @"(<title>.*?</title>)", RegexOptions.Singleline);
-                                    if (m1.Count > 0)
-                                    {
-                                        string value = m1[m1.Count - 1].Groups[1].Value;
-                                        if (!string.IsNullOrEmpty(value))
-                                        {
-                                            value = value.Replace(@"<title>", @"");
-                                            value = value.Replace(@"</title>", @"");
-                                            title = value.Trim();
-                                        }
-                                    }
-                                }
-                                bool isUnicode = AppGlobal.ContainsUnicodeCharacter(title);
-                                if ((title.Contains(@"&#") == true) || (isUnicode == false))
-                                {
-                                    MatchCollection m1 = Regex.Matches(htmlTitle, @"(<title>.*?</title>)", RegexOptions.Singleline);
-                                    if (m1.Count > 0)
-                                    {
-                                        string value = m1[m1.Count - 1].Groups[1].Value;
-                                        if (!string.IsNullOrEmpty(value))
-                                        {
-                                            value = value.Replace(@"<title>", @"");
-                                            value = value.Replace(@"</title>", @"");
-                                            title = value.Trim();
-                                        }
-                                    }
-                                }
-                                if (title.Split('|').Length > 2)
-                                {
-                                    title = title.Split('|')[1];
-                                }
-                                if (title.Split('|').Length > 1)
-                                {
-                                    title = title.Split('|')[0];
-                                }
-                                title = title.Trim();
-                                Product product = new Product();
-                                product.Description = "";
-                                product.Title = title;
-                                product.ParentID = config.ID;
-                                product.CategoryID = config.ID;
-                                product.Source = AppGlobal.SourceAuto;
-                                if (string.IsNullOrEmpty(product.Title))
-                                {
-                                    product.Title = linkItem.Text;
-                                }
-                                product.URLCode = linkItem.Href;
-                                product.DatePublish = DateTime.Now;
-                                product.Initialization(InitType.Insert, RequestUserID);
-                                product.DatePublish = DateTime.Now;
-                                AppGlobal.FinderContentAndDatePublish001(html, product);
-                                if ((product.DatePublish.Year > 2019) && (product.Active == true))
-                                {
-                                    if (!string.IsNullOrEmpty(product.Title))
-                                    {
-                                        product.Title = AppGlobal.Decode(product.Title);
-                                        product.MetaTitle = AppGlobal.SetName(product.Title);
-                                    }
-                                    if (!string.IsNullOrEmpty(product.Description))
-                                    {
-                                        product.Description = AppGlobal.Decode(product.Description);
-                                    }
-                                    _productRepository.InsertSingleItem(product);
-                                }
-                                response.Close();
-                                readStream.Close();
-                            }
-                        }
-                        catch (Exception e1)
-                        {
-                            string mes1 = e1.Message;
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                string mes = e.Message;
-            }
-            return "";
-        }
+        }        
         public async Task<string> AsyncInsertSingleItem(MatchCollection m1, Config config, LinkItem linkItem, string html001)
         {
             if (m1.Count > 0)
